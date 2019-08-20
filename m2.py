f3a3e340c5f0af7d5aa681a85ff3155c3d066e2b
Anjana0987 <anjana.rajasekhar1228@gmail.com>
2019-08-20 15:08:49 -0500

    Create m2.py

---
+++ m2.py
@@ -0,0 +1,18 @@
+'''from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+
+data = pd.read_csv('try.csv')
+text_data = data['text']
+
+vec = TfidfVectorizer(sublinear_tf=True, stop_words='english')
+X = vec.fit_transform(text_data)
+feature_names = vec.get_feature_names()
+dense = X.todense()
+denselist = dense.tolist()
+df = pd.DataFrame(denselist, columns=feature_names)
+
+S = cosine_similarity(X)
+
+print(df)'''
+
